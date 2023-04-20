5e93afea5aa981eb51f739f8b3cde696f02a9776
enricoderasmo21 <derasmo.enrico@gmail.com>
2023-04-20 17:02:57 +0200

    first step done

--- js/script.js
+++ js/script.js
@@ -1,20 +1,27 @@
-const { createApp } = vue;
+const { createApp } = Vue
 
 createApp({
     data(){
         return{
 
-
+            todos: [],
 
         }
     },
 
     methods:{
 
+        getJson(){
+            axios.get('./server.php').then(response => {
+                console.log(response.data);
+
+                this.todos = response.data;
+            });
+        }
     },
 
     mounted(){
-
+        this.getJson()
     },
-    
+
 }).mount('#app');
--- server.php
+++ server.php
@@ -1,2 +1,10 @@
 <?php
 
+$string = file_get_contents('todos.json');
+
+$todos = json_decode($string);
+
+header('Content-Type: application/json');
+
+
+echo json_encode($todos);
